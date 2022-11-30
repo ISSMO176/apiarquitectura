25176b418e114953d7a27f43301c33783d59183b
ISSMO176 <i.sanhueza@duocuc.cl>
2022-11-30 00:46:36 -0300

    models update

--- projects/models.py
+++ projects/models.py
@@ -1,41 +1,33 @@
 from django.db import models
 
+from projects.api import ProfesorViewSet
+
 # Create your models here.
 
 class Asignaturas(models.Model):
-    codigo = models.CharField(max_length=10)
-    nombre = models.CharField(max_length=50)
-    descripcion = models.CharField(max_length=200)
-    fechacreacion = models.DateTimeField(auto_created=True)
+    nombre = models.CharField(max_length=60)
+    fechacreacion = models.DateTimeField(auto_now_add=True)
+    estado = models.CharField(max_length=20)
+    def __str__(self):
+        return self.nombre
+
 
 class Alumnos(models.Model):
-    nombre = models.CharField(max_length=40)
-    apellido = models.CharField(max_length=40)
-    rut = models.CharField(max_length=9)
-    correo = models.CharField(max_length=200)
-    estado = models.CharField(max_length=150)
-
-class Pago(models.Model):
-    fechaPago = models.DateTimeField(auto_now_add=True)
-    idFormaPago = models.ForeignKey(Formapago, on_delete=models.PROTECT)
-    rutAlumno = models.ForeignKey(Alumno, on_delete=models.PROTECT)
-    valor = models.IntegerField()
-    comision = models.IntegerField()
+    rut = models.CharField(max_length=8)
+    nombre = models.CharField(max_length=20)
+    apellido = models.CharField(max_length=20)
+    fechacreacion = models.DateTimeField(auto_now_add=True)
     estado = models.CharField(max_length=20)
-    idAgenda = models.ForeignKey(Agenda, on_delete=models.PROTECT)
+
+    def nombre_rut(self):
+        return "{} {}, {}".format(self.nombre, self.apellido , self.rut)
+
+    def __str__(self):
+        return self.nombre_rut()
 
 class Formapago(models.Model):
     nombre = models.CharField(max_length=20)
     fechaCreacion = models.DateTimeField(auto_now_add=True)
-    estado = models.CharField(max_length=20)
-
-class Agenda(models.Model):
-    fechaHorario = models.DateTimeField(auto_now_add=True)
-    fechaHorarioFin = models.DateTimeField(auto_now_add=True)
-    idAsignatura = models.ForeignKey(Asignatura, on_delete=models.PROTECT)
-    datos_profe = models.ForeignKey(Profesor, on_delete=models.PROTECT)
-    idPlataforma = models.ForeignKey(Plataforma, on_delete=models.PROTECT)
-    link = models.TextField(max_length=100)
     estado = models.CharField(max_length=20)
 
 class Plataforma(models.Model):
@@ -43,6 +35,23 @@ class Plataforma(models.Model):
     fechacreacion = models.DateTimeField(auto_now_add=True)
     estado = models.CharField(max_length=20)
 
+class Agenda(models.Model):
+    fechaHorario = models.DateTimeField(auto_now_add=True)
+    fechaHorarioFin = models.DateTimeField(auto_now_add=True)
+    idAsignatura = models.ForeignKey(Asignaturas, on_delete=models.PROTECT)
+    datos_profe = models.ForeignKey(ProfesorViewSet, on_delete=models.PROTECT)
+    idPlataforma = models.ForeignKey(Plataforma, on_delete=models.PROTECT)
+    link = models.TextField(max_length=100)
+    estado = models.CharField(max_length=20)
+
+class Pago(models.Model):
+    fechaPago = models.DateTimeField(auto_now_add=True)
+    idFormaPago = models.ForeignKey(Formapago, on_delete=models.PROTECT)
+    rutAlumno = models.ForeignKey(Alumnos, on_delete=models.PROTECT)
+    valor = models.IntegerField()
+    comision = models.IntegerField()
+    estado = models.CharField(max_length=20)
+    idAgenda = models.ForeignKey(Agenda, on_delete=models.PROTECT)
 
 class Profesor(models.Model):
     rut = models.CharField(max_length=8)
@@ -55,3 +64,8 @@ class Profesor(models.Model):
 
     def __str__(self):
         return self.datos_profe()
+
+class Asistencia(models.Model):
+    rutAlumno = models.ForeignKey(Alumnos, on_delete=models.PROTECT, null=True)
+    estado = models.CharField(max_length=20)
+    fechaAsistencia = models.DateTimeField(auto_now_add=True)
